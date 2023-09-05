448a915084d006a12676a3dc3211e323e524b78d
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-09-05 23:17:19 +0200

    refact header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,58 +1,61 @@
 import "./Header.css";
 export const Header = () => `
 <div id="skip-link">
-
-  <div id="page-wrapper"><div id="page">
-
-    <div id="header"><div class="section clearfix">
-      <div class="page-container">
-
-        				<div class="site-logo">
-  				<h1 role="banner" class="site-header__banner">
-            <a href="index.html" title="Inicio" rel="home" id="logo">
-              <img src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931765/Xtalentodigital/logo_rumexl.png" alt="Inicio" />
-					</h1>
-				</div>
-        
-        <div class="menuresponsive"></div>
-
+<div id="page-wrapper">
+  <div id="page">
+    <div id="header">
+      <div class="section clearfix">
+        <div class="page-container">
+          <div class="site-logo">
+            <h1 role="banner" class="site-header__banner">
+              <a href="index.html" title="Inicio" rel="home" id="logo">
+                <img src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931765/Xtalentodigital/logo_rumexl.png" alt="Inicio" />
+            </h1>
+          </div>
+          <div class="menuresponsive">
+          </div>
           <div class="region region-pre-header">
-    <div id="block-block-3" class="block block-block bloque-redes-sociales">
-
-    
-  <div class="content">
-    <ul class="social-header"><li class="first"><a href="https://www.facebook.com/fundaciononce" target="_blank" title="Abre en nueva ventana"><img alt="Facebook (abre en ventana nueva)" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931821/Xtalentodigital/icon-facebook_vngqmk.png" /></a></li>
-<li class="first"><a href="https://twitter.com/Fundacion_ONCE" target="_blank" title="Abre en nueva ventana"><img alt="Twitter (abre en ventana nueva)" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931823/Xtalentodigital/icon-twitter_i34loe.png" /></a></li>
-<li class="first"><a href="https://www.youtube.com/user/FundacionONCE" target="_blank" title="Abre en nueva ventana"><img alt="Youtube (abre en ventana nueva)" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931821/Xtalentodigital/icon-youtube_j1akp6.png" /></a></li>
-<li class="first"><a href="https://www.instagram.com/fundaciononce/" target="_blank" title="Abre en nueva ventana"><img alt="Instagram (abre en ventana nueva)" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931822/Xtalentodigital/icon-instagram_xxffdw.png" /></a></li>
-<li class="first"><a href="https://es.linkedin.com/company/fundacion-once-inserta" target="_blank" title="Abre en nueva ventana"><img alt="LinkedIn (abre en ventana nueva)" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931823/Xtalentodigital/icon-linkedin_br0rai.png" /></a></li>
-</ul>  </div>
-</div>
-<div id="block-block-24" class="block block-block bloque-grupo-once">
-
-    
-  <div class="content">
-    <p style="margin:0;"><img alt="Grupo Social ONCE" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693932000/Xtalentodigital/logo-gso2021_ljz8gs.png" width="200" /></p>
-  </div>
-</div>
-  </div>
-
-        
-          <div class="region region-header">
-    <div id="block-menu-menu-superior" class="block block-menu">
-
-    
-  <div class="content">
-    <ul class="menu"><li class="first leaf"><a href="node/2.html">¿Qué es XTD?</a></li>
-<li class="leaf"><a href="cursos.html" title="">Cursos</a></li>
-<li class="collapsed"><a href="becas.html" title="">Becas</a></li>
-<li class="leaf"><a href="blog.html" title="">Blog</a></li>
-<li class="leaf"><a href="alumnos.html" title="">Alumni </a></li>
-<li class="last leaf"><a href="colaboradores.html">Colaboradores</a></li>
-</ul>  </div>
-</div>
-  </div>
-        
+            <div id="block-block-3" class="block block-block bloque-redes-sociales">
+              <div class="content">
+                <ul class="social-header">
+                  <li class="first"><a href="https://www.facebook.com/fundaciononce" target="_blank"><img alt="Facebook" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931821/Xtalentodigital/icon-facebook_vngqmk.png"/>
+                  </a></li>
+                  <li class="first"><a href="https://twitter.com/Fundacion_ONCE" target="_blank"><img alt="Twitter" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931823/Xtalentodigital/icon-twitter_i34loe.png"/>
+                  </a></li>
+                  <li class="first"><a href="https://www.youtube.com/user/FundacionONCE" target="_blank"><img alt="Youtube" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931821/Xtalentodigital/icon-youtube_j1akp6.png"/>
+                  </a></li>
+                  <li class="first"><a href="https://www.instagram.com/fundaciononce/" target="_blank"><img alt="Instagram" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931822/Xtalentodigital/icon-instagram_xxffdw.png"/>
+                  </a></li>
+                  <li class="first"><a href="https://es.linkedin.com/company/fundacion-once-inserta" target="_blank"><img alt="LinkedIn" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931823/Xtalentodigital/icon-linkedin_br0rai.png"/>
+                  </a></li>
+                </ul>  
+              </div>
+            </div>
+              <div id="block-block-24" class="block block-block bloque-grupo-once">
+                <div class="content">
+                  <p style="margin:0;"><img alt="Grupo Social ONCE" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693932000/Xtalentodigital/logo-gso2021_ljz8gs.png" width="200" /></p>
+                </div>
+              </div>
+            </div>
+            <div class="region region-header">
+              <div id="block-menu-menu-superior" class="block block-menu">
+                <div class="content">
+                  <ul class="menu">
+                    <li class="first leaf"><a href="node/2.html">¿Qué es XTD?</a></li>
+                    <li class="leaf"><a href="cursos.html" title="">Cursos</a></li>
+                    <li class="collapsed"><a href="becas.html" title="">Becas</a></li>
+                    <li class="leaf"><a href="blog.html" title="">Blog</a></li>
+                    <li class="leaf"><a href="alumnos.html" title="">Alumni </a></li>
+                    <li class="last leaf"><a href="colaboradores.html">Colaboradores</a></li>
+                  </ul>  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div></div>
+    </div>
+  </div>
+</div>
+</div>
 `;
