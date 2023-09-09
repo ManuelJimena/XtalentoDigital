1cad30a226936ed380a993946297b6184c5b2e46
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-09-09 18:21:34 +0200

    refact title

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -54,6 +54,341 @@ main.innerHTML = `
 				</div>
 
       </div></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <div class="panelcontent">
+  <div class="panelcontent-left">
+  <h3>¿Qué es el Programa Por Talento Digital?</h3>
+  <p><strong>Por Talento Digital es un programa de formación permanente en competencias digitales y profesiones tecnológicas de la <a href="https://www.fundaciononce.es" target="_blank" title="Abre en ventana nueva web Fundación ONCE">Fundación ONCE</a> </strong> orientado a la adquisición de conocimientos y cualificación tecnológica y digital de las personas con discapacidad para favorecer así su inclusión laboral en profesiones con alta demanda en el mercado de trabajo, multiplicando de esta forma sus perspectivas profesionales.</p>
+  </div>
+  <div class="panelcontent-right"><img alt="imagen de un pc" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931907/Xtalentodigital/banner_home_cujp1e.png" /><div class="convocatoria"><h3>II Convocatoria de becas</h3><p>¡Ya se ha abierto la II Convocatoria de becas de formación individual.
+  <a href="/becas"> No pierdas la oportunidad!</a></p></div>
+  </div>
+  </div>
+  
+
+
+    
+    </div>
+  <div class="panel-separator"></div><div class="panel-pane pane-views pane-cursos formacion_home"  >
+    
+          <h2 class="pane-title">
+        <span class="title">Cursos de formación</span>    </h2>
+      
+    
+    <div class="pane-content">
+      <div class="view view-cursos view-id-cursos view-display-id-block view-dom-id-cc86f3895d15919f2acdcdacedc34685">
+          
+    
+    
+        <div class="view-content">
+        <div class="listado_cursos_home">    <ul>          <li class="">  
+    <div class="views-field views-field-nothing">        <span class="field-content"><div class="wrapper-curso-item">
+      <div class="bloque-fecha">
+          <span>Inscripción</span>
+          <span>hasta el</span>
+          <span class="item-dia"><span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2023-12-31T00:00:00+01:00">31</span></span>
+          <span class="item-fecha"><span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2023-12-31T00:00:00+01:00">Dic 2023</span></span>
+      </div>
+      <div class="bloque-contenido">
+          <!-- field_ultimas_plazas -->
+          
+  <div class="inscripcion-wrapper">
+      <a href="https://portalentodigital.fundaciononce.es/inscripcion?t=competenciasbasicasgeneral-PREPARATEPARAELMUNDODIGITAL&n=1139" target="_blank" class="btn-inscribite" title="Inscríbete a Prepárate para el mundo digital,  en Zona Embajadores">Inscríbete</a>
+  </div> 
+          <div class="bloque-titulo">
+              <h3><a href="/cursos/preparate-para-el-mundo-digital">Prepárate para el mundo digital</a></h3>
+          </div>
+          <div class="bloque-texto">
+              <div class="bloque-texto-1">
+                  <div class="fila">
+                      <!-- <span class="etiqueta">Fecha inicio:</span> -->
+                  </div>
+                  <div class="fila">
+                      <!-- <span class="etiqueta">Fecha fin:</span> -->
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Lugar:</span>Zona Embajadores (Madrid)
+                  </div>
+              </div>
+              <div class="bloque-texto-2">
+                  <div class="fila">
+                      <span class="etiqueta">Duración:</span>
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Plazas:</span>
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Modalidad:</span>Presencial
+                  </div>
+              </div> 
+          </div>
+          <div class="bloque-requisitos">
+              <span class="etiqueta">Requisitos:</span>
+              <p>Sujeto a configuración de grupo completo.</p>
+   
+          </div>
+          <div class="bloque-descripcion">
+              <span class="etiqueta">Descripción:</span>
+              <p>El itinerario formativo <strong>"Prepárate para el mundo...</strong></p> 
+          </div>
+      </div>
+  </div></span>  </div></li>
+            <li class="">  
+    <div class="views-field views-field-nothing">        <span class="field-content"><div class="wrapper-curso-item">
+      <div class="bloque-fecha">
+          <span>Inscripción</span>
+          <span>hasta el</span>
+          <span class="item-dia"><span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2023-09-15T00:00:00+02:00">15</span></span>
+          <span class="item-fecha"><span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2023-09-15T00:00:00+02:00">Sep 2023</span></span>
+      </div>
+      <div class="bloque-contenido">
+          <!-- field_ultimas_plazas -->
+          
+  <div class="inscripcion-wrapper">
+      <a href="https://portalentodigital.fundaciononce.es/inscripcion?t=FrontendAvanzadoRemoto&n=1609" target="_blank" class="btn-inscribite" title="Inscríbete a Desarrollador/a Web Frontend Avanzado en Remoto,  en Toda España">Inscríbete</a>
+  </div> 
+          <div class="bloque-titulo">
+              <h3><a href="/cursos/desarrolladora-web-frontend-avanzado-en-remoto">Desarrollador/a Web Frontend Avanzado en Remoto</a></h3>
+          </div>
+          <div class="bloque-texto">
+              <div class="bloque-texto-1">
+                  <div class="fila">
+                      <!-- <span class="etiqueta">Fecha inicio:</span> -->
+                  </div>
+                  <div class="fila">
+                      <!-- <span class="etiqueta">Fecha fin:</span> -->
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Lugar:</span>Toda España ()
+                  </div>
+              </div>
+              <div class="bloque-texto-2">
+                  <div class="fila">
+                      <span class="etiqueta">Duración:</span>500 horas
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Plazas:</span>
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Modalidad:</span>A distancia
+                  </div>
+              </div> 
+          </div>
+          <div class="bloque-requisitos">
+              <span class="etiqueta">Requisitos:</span>
+              <p>Conocimientos básicos sobre HTML, CSS y Javascript</p>
+   
+          </div>
+          <div class="bloque-descripcion">
+              <span class="etiqueta">Descripción:</span>
+              <p>La formación <strong>"Desarrollador/a Web Frontend...</strong></p> 
+          </div>
+      </div>
+  </div></span>  </div></li>
+            <li class="">  
+    <div class="views-field views-field-nothing">        <span class="field-content"><div class="wrapper-curso-item">
+      <div class="bloque-fecha">
+          <span>Inscripción</span>
+          <span>hasta el</span>
+          <span class="item-dia"><span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2023-09-11T00:00:00+02:00">11</span></span>
+          <span class="item-fecha"><span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2023-09-11T00:00:00+02:00">Sep 2023</span></span>
+      </div>
+      <div class="bloque-contenido">
+          <!-- field_ultimas_plazas -->
+          
+  <div class="inscripcion-wrapper">
+      <a href="https://portalentodigital.fundaciononce.es/inscripcion?t=cursoPuestodetrabajociberseguroIIED&n=1218" target="_blank" class="btn-inscribite" title="Inscríbete a Itinerario básico de Experto en Ciberseguridad- II Edición, 14 Sep 2023 en Toda España">Inscríbete</a>
+  </div> 
+          <div class="bloque-titulo">
+              <h3><a href="/cursos/itinerario-basico-de-experto-en-ciberseguridad-ii-edicion">Itinerario básico de Experto en Ciberseguridad- II Edición</a></h3>
+          </div>
+          <div class="bloque-texto">
+              <div class="bloque-texto-1">
+                  <div class="fila">
+                      <!-- <span class="etiqueta">Fecha inicio:</span>14 Sep 2023 -->
+                  </div>
+                  <div class="fila">
+                      <!-- <span class="etiqueta">Fecha fin:</span> -->
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Lugar:</span>Toda España ()
+                  </div>
+              </div>
+              <div class="bloque-texto-2">
+                  <div class="fila">
+                      <span class="etiqueta">Duración:</span>250 horas
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Plazas:</span>100
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Modalidad:</span>A distancia
+                  </div>
+              </div> 
+          </div>
+          <div class="bloque-requisitos">
+              <span class="etiqueta">Requisitos:</span>
+              <p>Conocimientos de informática y/o ofimática nivel medio...</p> 
+          </div>
+          <div class="bloque-descripcion">
+              <span class="etiqueta">Descripción:</span>
+              <p>Con el<strong> Itinerario básico de Experto en...</strong></p> 
+          </div>
+      </div>
+  </div></span>  </div></li>
+            <li class="">  
+    <div class="views-field views-field-nothing">        <span class="field-content"><div class="wrapper-curso-item">
+      <div class="bloque-fecha">
+          <span>Inscripción</span>
+          <span>hasta el</span>
+          <span class="item-dia"><span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2023-09-15T00:00:00+02:00">15</span></span>
+          <span class="item-fecha"><span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2023-09-15T00:00:00+02:00">Sep 2023</span></span>
+      </div>
+      <div class="bloque-contenido">
+          <!-- field_ultimas_plazas -->
+          
+  <div class="inscripcion-wrapper">
+      <a href="https://portalentodigital.fundaciononce.es/inscripcion?t=cursoInglespresencialtrimestral3edicion&n=1621" target="_blank" class="btn-inscribite" title="Inscríbete a English for Digital Workers III Edición, 18 Sep 2023 en Zona Embajadores">Inscríbete</a>
+  </div> 
+          <div class="bloque-titulo">
+              <h3><a href="/cursos/english-digital-workers-iii-edicion">English for Digital Workers III Edición</a></h3>
+          </div>
+          <div class="bloque-texto">
+              <div class="bloque-texto-1">
+                  <div class="fila">
+                      <!-- <span class="etiqueta">Fecha inicio:</span>18 Sep 2023 -->
+                  </div>
+                  <div class="fila">
+                      <!-- <span class="etiqueta">Fecha fin:</span> -->
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Lugar:</span>Zona Embajadores (Madrid)
+                  </div>
+              </div>
+              <div class="bloque-texto-2">
+                  <div class="fila">
+                      <span class="etiqueta">Duración:</span>36 horas
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Plazas:</span>
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Modalidad:</span>Presencial
+                  </div>
+              </div> 
+          </div>
+          <div class="bloque-requisitos">
+              <span class="etiqueta">Requisitos:</span>
+              <p>Configuración de grupo tras realización de prueba de...</p> 
+          </div>
+          <div class="bloque-descripcion">
+              <span class="etiqueta">Descripción:</span>
+              <p>La acción formativa de Inglés especializado en...</p> 
+          </div>
+      </div>
+  </div></span>  </div></li>
+            <li class="">  
+    <div class="views-field views-field-nothing">        <span class="field-content"><div class="wrapper-curso-item">
+      <div class="bloque-fecha">
+          <span>Inscripción</span>
+          <span>hasta el</span>
+          <span class="item-dia"><span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2023-09-15T00:00:00+02:00">15</span></span>
+          <span class="item-fecha"><span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2023-09-15T00:00:00+02:00">Sep 2023</span></span>
+      </div>
+      <div class="bloque-contenido">
+          <!-- field_ultimas_plazas -->
+          
+  <div class="inscripcion-wrapper">
+      <a href="https://portalentodigital.fundaciononce.es/inscripcion?t=cursoDISENOGRAFICOnivelAvanzado&n=1569" target="_blank" class="btn-inscribite" title="Inscríbete a Diseño gráfico nivel avanzado (Paquete Adobe), 19 Sep 2023 en Zona Embajadores">Inscríbete</a>
+  </div> 
+          <div class="bloque-titulo">
+              <h3><a href="/cursos/diseno-grafico-nivel-avanzado-paquete-adobe">Diseño gráfico nivel avanzado (Paquete Adobe)</a></h3>
+          </div>
+          <div class="bloque-texto">
+              <div class="bloque-texto-1">
+                  <div class="fila">
+                      <!-- <span class="etiqueta">Fecha inicio:</span>19 Sep 2023 -->
+                  </div>
+                  <div class="fila">
+                      <!-- <span class="etiqueta">Fecha fin:</span> -->
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Lugar:</span>Zona Embajadores (Madrid)
+                  </div>
+              </div>
+              <div class="bloque-texto-2">
+                  <div class="fila">
+                      <span class="etiqueta">Duración:</span>60 horas
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Plazas:</span>15
+                  </div>
+                  <div class="fila">
+                      <span class="etiqueta">Modalidad:</span>Presencial
+                  </div>
+              </div> 
+          </div>
+          <div class="bloque-requisitos">
+              <span class="etiqueta">Requisitos:</span>
+              <p>Superar con éxito la prueba de nivel o haber sido apto/a...</p> 
+          </div>
+          <div class="bloque-descripcion">
+              <span class="etiqueta">Descripción:</span>
+              <p>La formación de <strong>"Diseño gráfico" nivel avanzado...</strong></p> 
+          </div>
+      </div>
+  </div></span>  </div></li>
+        </ul></div>    </div>
+    
+    
+    
+    
+        <div class="view-footer">
+        <div class="presentacion">
+  <a href="/cursos"><span>Ver el listado completo de cursos y talleres de formación</span><br /><span>Ir a Cursos de formación</span></a>
+  </div>
+      </div>
+    
+    
+  </div>  </div>
+  
+    
+    </div>
+    </div>
+  </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       `;
 
       document.addEventListener("DOMContentLoaded", function () {
