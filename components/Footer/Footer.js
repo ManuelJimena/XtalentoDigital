a51d288e3ead8554c7c1e8083bfd1d7993965ac8
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-09-10 15:47:36 +0200

    refact footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import "./Footer.css";
 export const Footer = () => `
-<div class="infoxtalento"><a href="https://portalentodigital.fundaciononce.es/accesibilidad" target="_blank">¿Quieres recibir más información sobre los cursos y becas Por Talento Digital?</a></div>
+<div class="infoxtalento"><a href="https://portalentodigital.fundaciononce.es/accesibilidad" target="_blank">¿Quieres recibir más información sobre los cursos y becas Por Talento Digital?</a><img class="dedoclick" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1694353042/Xtalentodigital/144939537-al-hacer-clic-en-el-icono-del-dedo-puntero-de-mano-haga-clic-con-el-rat%C3%B3n_p10lyn.png" alt="logo click"></div>
 <container class="footer">
     <div class="address">
       <p>C/Fray Luis de León, 11</p>
@@ -18,8 +18,11 @@ export const Footer = () => `
     <li><a href="https://portalentodigital.fundaciononce.es/politica-de-cookies" target="_blank">Politica de cookies</a></li>
     </ul>
     </div>
+    <img class="gsonce" src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1694014784/Xtalentodigital/logo-gso2022_iyfxv0.png" alt="logo grupo social once">
     </container>
-    <div class="socialnav"> 
+    <div class="dgeneration"><a href="https://generaciond.gob.es/" target="_blank"><img src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1693931999/Xtalentodigital/generaciond_a3zjwi.png" alt="logo generacion D"></a>
+    </div>
+    <div class="socialfooter"> 
         <ul>
         <li><a
             class="facebook"
