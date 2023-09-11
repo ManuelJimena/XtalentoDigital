14574fe81a39e75ccc74c6158529543ac2b1a47d
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-09-11 17:57:43 +0200

    refact pages

--- pages/Becas/Becas.js
+++ pages/Becas/Becas.js
@@ -4,5 +4,15 @@ export const Becas = () => {
 const main = document.querySelector("main");
 cleanPage(main);
 main.innerHTML = `
+<div class="treasure">
+    <div class="treasure-animation">
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<lottie-player src="https://assets9.lottiefiles.com/packages/lf20_W2mq7z.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop autoplay></lottie-player>
+  </div>
+  
+  <h2>
+¡Enhorabuena, has encontrado un tesoro!
+  </h2>
+  </div>
 `;
 };
